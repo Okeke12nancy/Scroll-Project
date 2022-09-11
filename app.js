453594e891172ca1215b8a46ecb94a19364509c4
Nancy Okeke <ogadimmanancy12@gmail.com>
2022-09-11 05:17:04 -0700

    Scroll Project

---
+++ app.js
@@ -0,0 +1,63 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+// ********** set date ************
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+// ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+// ********** fixed navbar ************
+navToggle.addEventListener("click", function () {
+    // linksContainer.classList.toggle("show-links");
+const containerHeight = linksContainer.getBoundingClientRect().height;
+const linksHeight = links.getBoundingClientRect().height;
+
+if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px` ;
+} else {
+    linksContainer.style.height = 0;
+}
+});
+
+const toplink = document.querySelector(".top-link");
+// ********** fixed navbar **************8
+window/addEventListener('scroll', function(){
+    const scrollHeight = window.pageYOffset;
+    const newHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+
+    if (scrollHeight > 500) {
+        toplink.classList.add("show-link");
+    } else {
+        toplink.classList.add("show-link");
+    }
+});
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelector('.scroll-link');
+ scrollLinks.forEach(function (link) {
+    link.addEventListener("click", function (e){
+         
+    })
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
